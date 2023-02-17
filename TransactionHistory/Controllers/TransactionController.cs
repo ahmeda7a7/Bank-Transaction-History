3cafe04a99b3c0ad825d61648c8b4414c06eed45
ahmeda7a7 <azazhumdoon3@gmail.com>
2023-02-17 19:28:18 +0600

    all the project files are uploaded

---
+++ TransactionHistory/Controllers/TransactionController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TransactionHistory.Models;
+using static TransactionHistory.Helper;
+
+namespace TransactionHistory.Controllers
+{
+    public class TransactionController : Controller
+    {
+        private readonly TransactionDbContext _context;
+
+        public TransactionController(TransactionDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Transaction
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Transactions.ToListAsync());
+        }
+
+
+
+		// GET: Transaction/AddOrEdit(Insert)
+		// GET: Transaction/AddOrEdit/5(Update)
+		[NoDirectAccess]
+		public async Task<IActionResult> AddOrEdit(int id = 0)
+        {
+            if (id == 0)
+                return View(new TransactionModel());
+            else
+            {
+                var transactionModel = await _context.Transactions.FindAsync(id);
+                if (transactionModel == null)
+                {
+                    return NotFound();
+                }
+                return View(transactionModel);
+            }
+        } 
+
+        /*inside this post action method we will handle form submit event*/
+        /*// POST: Transaction/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("TransactionId,AccountNumber,BeneficiaryName,BankName,SWIFTCode,Amount,Date")] TransactionModel transactionModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(transactionModel);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(transactionModel);
+        }*/
+
+        /*similarly we have get and post action method for edit */
+        // GET: Transaction/Edit/5
+        /*public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Transactions == null)
+            {
+                return NotFound();
+            }
+
+            var transactionModel = await _context.Transactions.FindAsync(id);
+            if (transactionModel == null)
+            {
+                return NotFound();
+            }
+            return View(transactionModel);
+        }*/
+
+        // POST: Transaction/AddOrEdit
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddOrEdit(int id, [Bind("TransactionId,AccountNumber,BeneficiaryName,BankName,SWIFTCode,Amount,Date")] TransactionModel transactionModel)
+        {
+            
+
+            if (ModelState.IsValid)
+            {
+                if (id == 0)
+                {
+                    transactionModel.Date = DateTime.Now;
+                    _context.Add(transactionModel);
+                    await _context.SaveChangesAsync();
+
+                }
+                else
+                {
+                    try
+                    {
+                        _context.Update(transactionModel);
+                        await _context.SaveChangesAsync();
+                    }
+                    catch (DbUpdateConcurrencyException)
+                    {
+                        if (!TransactionModelExists(transactionModel.TransactionId))
+                        { return NotFound(); }
+                        else
+                        { throw; }
+                    }
+                }
+                return Json(new { isValid = true, html = Helper.RenderRazorViewToString(this, "_ViewAll", _context.Transactions.ToList()) });
+            }
+            return Json(new { isValid = false, html = Helper.RenderRazorViewToString(this, "AddOrEdit", transactionModel) });
+        }
+
+        // GET: Transaction/Delete/5
+        /*public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Transactions == null)
+            {
+                return NotFound();
+            }
+
+            var transactionModel = await _context.Transactions
+                .FirstOrDefaultAsync(m => m.TransactionId == id);
+            if (transactionModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(transactionModel);
+        }*/
+
+        // POST: Transaction/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+			var transactionModel = await _context.Transactions.FindAsync(id);
+			_context.Transactions.Remove(transactionModel);
+			await _context.SaveChangesAsync();
+			return Json(new { html = Helper.RenderRazorViewToString(this, "_ViewAll", _context.Transactions.ToList()) });
+		}
+
+        private bool TransactionModelExists(int id)
+        {
+            return (_context.Transactions?.Any(e => e.TransactionId == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ TransactionHistory/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TransactionHistory.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
